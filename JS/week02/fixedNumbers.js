b6545b72188e8fd13c779c9dbdf1105c81124977
avgulmerve <124457115+avgulmerve@users.noreply.github.com>
2023-03-16 10:08:04 +0100

    fixedNumbers.js

---
+++ JS/week02/fixedNumbers.js
@@ -0,0 +1,13 @@
+/*5*/ //How do you round the number 7.25, to the nearest integer (i.e., whole number)?
+
+
+let z = 7.25;
+console.log(z);
+a= z.toFixed(0);
+console.log(a);
+if( z>a) 
+  theHighestValue = z;
+else
+  theHighestValue=a;
+
+console.log(theHighestValue);
