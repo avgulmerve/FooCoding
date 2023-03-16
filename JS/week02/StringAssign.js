418837d204186e6f4036f819ba8783aac9c6bbdf
avgulmerve <124457115+avgulmerve@users.noreply.github.com>
2023-03-16 10:05:07 +0100

    StringAssign.js

---
+++ JS/week02/StringAssign.js
@@ -0,0 +1,9 @@
+/*4*/ //Declare a variable y and assign a string to it.
+
+
+let y="Almira";
+console.log(`My nephew name is: ${y}`);
+console.log(y);
+y="Alin"
+console.log(`My nephew name is :${y}`);
+console.log(y);
