edd5921e1000eccc484c805f6eded4f32fa9ab5f
avgulmerve <124457115+avgulmerve@users.noreply.github.com>
2023-03-16 09:53:41 +0100

    stringInitialize.js

---
+++ JS/week02/stringInitialize.js
@@ -0,0 +1,9 @@
+/*3*/ //Declare a variable x and initialize it with an integer, using these exact steps:
+
+
+let x;
+console.log("the value of my variable x will be: my old city registiration number");
+console.log(x);
+x = 34;
+console.log(`the value of my variable ${x} is: my old city registiration number`);
+console.log(x);
