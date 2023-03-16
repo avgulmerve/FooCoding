397c397da569cb9ee5f6f2f89cc8d12f6dc55391
avgulmerve <124457115+avgulmerve@users.noreply.github.com>
2023-03-16 10:09:43 +0100

    arraysPractice.js

---
+++ JS/week02/arraysPractice.js
@@ -0,0 +1,12 @@
+/*6*/ //Arrays
+
+
+let favoriteMeals = [];
+console.log("My favorite meals are");
+console.log(favoriteMeals);
+
+const favoriteAnimals = ['cats', 'dogs', 'penguins'];
+console.log (favoriteAnimals);
+let DaanFavoriteAnimal = "babypig";
+favoriteAnimals.unshift('baby pig');
+console.log(favoriteAnimals)
