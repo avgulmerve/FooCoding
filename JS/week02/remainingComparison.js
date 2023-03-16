4aac599d35ff0df15e00f51c398687b4dad04325
avgulmerve <124457115+avgulmerve@users.noreply.github.com>
2023-03-16 10:14:28 +0100

    remainingComparison.js

---
+++ JS/week02/remainingComparison.js
@@ -0,0 +1,22 @@
+/*9*/ //If x equals 7, and the only other statement is x = x % 3, what would be the new value of x?
+
+
+let r=7;
+r=r%3;
+console.log(r);
+
+let p=10
+p=p%5
+console.log("new p variable is equal remaining of ten division five: " + p)
+
+z=20;
+if(z%2==0)
+  console.log("z is even");
+else
+  console.log("z is odd");
+
+f=25;
+if(f%5==0)
+  console.log("f is multiple of 5")
+else
+  console.log("f is not multiple of 5")
