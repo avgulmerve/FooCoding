37d5997faac475676541586cfe80ff17db90a314
avgulmerve <124457115+avgulmerve@users.noreply.github.com>
2023-06-11 00:26:57 +0200

    Update app.js

--- Database/week03/app.js
+++ Database/week03/app.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 
 const app = express();
 //var router = express.Router();
-const port = 3000;
+const port = 8080;
 
 const connection = mysql.createConnection ({
     host: 'localhost',
